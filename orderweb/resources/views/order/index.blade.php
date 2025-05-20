d8a9e05f2bde8b23fc2c843b8c0491d707840312
nicolle <nicollev2020@gmail.com>
2025-05-20 12:32:51 -0500

    Final vistas causal y controller
    se termina las vistas causal y su controlador

--- orderweb/resources/views/order/index.blade.php
+++ orderweb/resources/views/order/index.blade.php
@@ -1,12 +1,12 @@
 @extends('templates.base')
-@section('title', 'Crear causal')
-@section('header', 'Crear causal')
+@section('title', 'Crear órdenes')
+@section('header', 'Órdenes')
 @section('content')
     
     
     <div class="row">
         <div class="col-lg-12 mb-4 d-grid gap-2 d-md-block">
-            <a href="{{ route('causal.create') }}" class="btn btn-primary">Crear</a>
+            <a href="{{ route('order.create') }}" class="btn btn-primary">Crear</a>
         </div>
     </div>
 
@@ -19,26 +19,29 @@
                 <thead>
                     <tr>
                         <th>Id</th>
-                        <th>Descripción</th>
-                        <th>Acciones</th>
+                        <th>Fecha legalización</th>
+                        <th>Dirección</th>
+                        <th>Ciudad</th>
+                        <th>Causal</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($causals as $causal)      
                     <tr>
-                        <td>{{ $causal["id"] }}</td>
-                        <td>{{ $causal["description"] }}</td>
+                        <td>1</td>
+                        <td>2025-05-20</td>
+                        <td>callejon 7 muertos</td>
+                        <td>TULUÁ</td>
+                        <td>Suspencion</td>
                         <td>
-                            <a href="{{ route('causal.edit', $causal['id']) }}" title="editar" class="btn btn-primary btn-circle btn-sm">
+                            <a href="#" title="editar" class="btn btn-primary btn-circle btn-sm">
                                 <i class="far fa-edit"></i>
                             </a>
-                            <a href="{{ route('causal.destroy', $causal['id']) }}" title="eliminar" class="btn btn-danger btn-circle btn-sm"
+                            <a href="#" title="eliminar" class="btn btn-danger btn-circle btn-sm"
                             onclick="return remove();">
                                 <i class="fas fa-trash"></i>
                             </a>
                         </td>
                     </tr>
-                    @endforeach
                 </tbody>
             </table>
         </div>
--- orderweb/resources/views/templates/nav.blade.php
+++ orderweb/resources/views/templates/nav.blade.php
@@ -38,8 +38,8 @@
                     </a>
                     <div id="collapse1" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Consultar</a>
-                            <a class="collapse-item" href="#">Crear</a>                        
+                            <a class="collapse-item" href="{{ route('order.index') }}">Consultar</a>
+                            <a class="collapse-item" href="{{ route('order.index') }}">Crear</a>                        
                         </div>
                     </div>
                 </li> 
@@ -52,11 +52,13 @@
                     </a>
                     <div id="collapse2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Consultar</a>
-                            <a class="collapse-item" href="#">Crear</a>
+                            <a class="collapse-item" href="{{ route('causal.index') }}">Consultar</a>
+                            <a class="collapse-item" href="{{ route('causal.create') }}">Crear</a>
                         </div>
                     </div>
-                </li>             
+                </li>     
+                
+                
 
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse3"
@@ -66,8 +68,8 @@
                     </a>
                     <div id="collapse3" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Consultar</a>
-                            <a class="collapse-item" href="#">Crear</a>
+                            <a class="collapse-item" href="{{ route('observation.index') }}">Consultar</a>
+                            <a class="collapse-item" href="{{ route('observation.create') }}">Crear</a>
                         </div>
                     </div>
                 </li>
@@ -91,8 +93,8 @@
                     </a>
                     <div id="collapse4" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Consultar</a>
-                            <a class="collapse-item" href="#">Crear</a>
+                            <a class="collapse-item" href="{{ route('activity.index') }}">Consultar</a>
+                            <a class="collapse-item" href="{{ route('activity.create') }}">Crear</a>
                         </div>
                     </div>
                 </li>
@@ -105,8 +107,8 @@
                     </a>
                     <div id="collapse5" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Consultar</a>
-                            <a class="collapse-item" href="#">Crear</a>
+                            <a class="collapse-item" href="{{ route('type_activity.index') }}">Consultar</a>
+                            <a class="collapse-item" href="{{ route('type_activity.create') }}">Crear</a>
                         </div>
                     </div>
                 </li>
@@ -129,8 +131,8 @@
                     </a>
                     <div id="collapse6" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Consultar</a>
-                            <a class="collapse-item" href="#">Crear</a>
+                            <a class="collapse-item" href="{{ route('technician.index') }}">Consultar</a>
+                            <a class="collapse-item" href="{{ route('technician.create') }}">Crear</a>
                         </div>
                     </div>
                 </li>
